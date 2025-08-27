e237bef864111fe5440e180e9634a52723c89a6e
alexandr <aleofob@gmail.com>
2025-08-27 12:43:55 +0500

    stats update: func to convert dict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from stats import words_counter
-
 from stats import letter_count
+from stats import dict_to_list_of_dict
 
 def get_book_text(path_to_book):
     with open(path_to_book) as f:
@@ -8,8 +8,20 @@ def get_book_text(path_to_book):
     return text_of_book
 
 def main():
-    letters_dictionary = letter_count(get_book_text('books/frankenstein.txt'))
-    words_counter(get_book_text('books/frankenstein.txt'))
-    print(letters_dictionary)
+    path = 'books/frankenstein.txt'
+    letters_dictionary = letter_count(get_book_text(path)) 
+    list_of_dict = dict_to_list_of_dict(letters_dictionary)
+    print('============ BOOKBOT ============')
+    print(f'Analyzing book found at {path}...')
+    print('----------- Word Count ----------')
+    print(f'Found {words_counter(get_book_text(path))} total words')
+    print('--------- Character Count -------')
+    for i in list_of_dict:
+        if i['char'].isalpha():
+            char = i['char']
+            num = i['num']
+            print(f'{char}: {num}')
+    print('============= END ===============')
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,7 @@
 def words_counter(text):
     list_of_words = text.split()
     count_of_words = len(list_of_words)
-    print(f"{count_of_words} words found in the document")
+    return count_of_words
 
 def letter_count(text):
     letters_dict = {}
@@ -12,3 +12,16 @@ def letter_count(text):
         else:
             letters_dict[letter] = 1
     return letters_dict
+
+def dict_to_list_of_dict(dict1):
+    def sort_on(items):
+        return items["num"]
+    
+    list_of_dict = []
+    for key in dict1:
+        tempo_dict = {}
+        tempo_dict["char"] = key
+        tempo_dict["num"] = dict1[key]
+        list_of_dict.append(tempo_dict)
+    list_of_dict.sort(reverse=True, key = sort_on)
+    return list_of_dict
