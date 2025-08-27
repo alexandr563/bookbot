ff5103b808b134182a28b92163327adb203f06b0
alexandr <aleofob@gmail.com>
2025-08-27 13:09:11 +0500

    add path_to_book  choosing

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import sys
+
 from stats import words_counter
 from stats import letter_count
 from stats import dict_to_list_of_dict
+
+sys_arg = sys.argv
+if not len(sys_arg) == 2:
+    print('Usage: python3 main.py <path_to_book>')
+    sys.exit(1)
+
+path = sys_arg[1]
 
 def get_book_text(path_to_book):
     with open(path_to_book) as f:
@@ -8,7 +17,6 @@ def get_book_text(path_to_book):
     return text_of_book
 
 def main():
-    path = 'books/frankenstein.txt'
     letters_dictionary = letter_count(get_book_text(path)) 
     list_of_dict = dict_to_list_of_dict(letters_dictionary)
     print('============ BOOKBOT ============')
